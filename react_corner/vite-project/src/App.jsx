147be38b6fff04de3da5258fcf696ad3f9e857ed
faysal-hossain11 <devfaysalbd@gmail.com>
2025-07-13 23:07:40 +0600

    learn react

--- react_corner/vite-project/src/App.jsx
+++ react_corner/vite-project/src/App.jsx
@@ -1,14 +1,27 @@
-import React, { Profiler } from 'react';
+import React, { Profiler, useEffect, useState } from 'react';
 import AvoidContradiction from './components/AvoidConcradiction';
 
 const App = () => {
 
+  const [user, setUser] = useState([])
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res) => res.json())
+    .then((data) => setUser(data));
+    console.log("asdfasdf",user);
+    
+  }, [])
+
   return (
     <>
 
-      <AvoidContradiction />
+      {/* <AvoidContradiction /> */}
       {/* <Profiler /> */}
 
+      <div>
+        <h3>useEffect</h3>
+      </div>
     </>
   );
 };
