9bf0523ff834ae2f684e31d100f916c2208329a5
faysal-hossain11 <devfaysalbd@gmail.com>
2025-08-02 08:14:17 +0600

    some update

---
+++ recap.js
@@ -0,0 +1,20 @@
+// const fruits = ["faysal", "towhid", "jahid", "sadik", "aslam"];
+
+// fruits[2] = "saddam" 
+// console.log(fruits)
+
+
+// const touris = ['sylhet', 'satkhira', 'chatpur']
+// touris.push('bandorbon');
+// touris.push('khulna', 'borishal')
+// touris.pop()
+// console.log(touris);
+
+
+// const books = ['bangla', 'english', 'match', 'physics', 'chamictry']
+// console.log(books.includes('english'));
+
+
+
+
+
