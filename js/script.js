6b9a13cf7694c36f1acdfae1eb4a10a5613ced84
faysal-hossain11 <devfaysalbd@gmail.com>
2025-08-21 00:14:39 +0600

    some update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
 document.getElementById("post_btn").addEventListener("click", function() {
-    console.log("clicked!");
-    location.reload(); // manually reload page
+
+    const textValue = document.getElementById("post_text");
+    const newComment = textValue.value;
+    
+    const parent = document.getElementById("comments-container");
+    
+    const createElement = document.createElement("p");
+    createElement.classList.add("paragraph")
+    createElement.innerText = newComment;
+
+    parent.appendChild(createElement)
+
+    textValue.value = " "
+    
 });
