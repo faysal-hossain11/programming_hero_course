e60ccd1eacd6a22b5abcd2ae643f944eb365a5c0
faysal-hossain11 <devfaysalbd@gmail.com>
2025-08-23 14:37:24 +0600

    some update

--- js/script.js
+++ js/script.js
@@ -1,16 +1,32 @@
-document.getElementById("post_btn").addEventListener("click", function() {
+// document.getElementById("post_btn").addEventListener("click", function() {
 
-    const textValue = document.getElementById("post_text");
-    const newComment = textValue.value;
-    
-    const parent = document.getElementById("comments-container");
-    
-    const createElement = document.createElement("p");
-    createElement.classList.add("paragraph")
-    createElement.innerText = newComment;
+//     const textValue = document.getElementById("post_text");
+//     const newComment = textValue.value;
 
-    parent.appendChild(createElement)
+//     const parent = document.getElementById("comments-container");
 
-    textValue.value = " "
-    
-});
+//     const createElement = document.createElement("p");
+//     createElement.classList.add("paragraph")
+//     createElement.innerText = newComment;
+
+//     parent.appendChild(createElement)
+
+//     textValue.value = " "
+
+// });
+
+
+
+function generateRandomColor() {
+    let maxVal = 0xFFFFFF; // 16777215 (decimal equivalent of FFFFFF)
+    let randomNumber = Math.random() * maxVal;
+    randomNumber = Math.floor(randomNumber);
+    let hexColor = randomNumber.toString(16); // Convert to hexadecimal
+    // Pad with leading zeros if necessary to ensure 6 characters
+    hexColor = hexColor.padStart(6, '0');
+    return `#${hexColor.toUpperCase()}`;
+}
+
+document.getElementById("body_bg").addEventListener("click", function () {
+    document.body.style.backgroundColor  = generateRandomColor();
+})
